5a551af20a623d8952b5db151ffd2f37d994eee2
sunjus <sunju.shin1108@gmail.com>
2020-06-04 17:26:45 +0300

    Solve ex1_innerHTML

---
+++ exercises/ex1/script.js
@@ -0,0 +1,23 @@
+document.querySelector('#shortcut').onclick=function(){
+    document.querySelector('#shortcut').innerHTML =  "succeed";
+}
+
+
+/* extensive version
+let mydom = document.querySelector('#shortcut')
+mydom.onclick = function (){
+    mydom.innerHTML = "succeed"
+}
+*/
+
+
+/* David's code
+function mainMessage(){
+    document.querySelector('#title').innerHTML='Hello Class';
+}
+function myFunction(){
+    document.getElementById("demo").innerHTML="hello World";
+}
+*/
+
+ 
