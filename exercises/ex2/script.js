e4220117b0f56a090c46733dd0dc83d9d4d5013b
sunjus <sunju.shin1108@gmail.com>
2020-06-04 18:04:57 +0300

    Solve ex2

---
+++ exercises/ex2/script.js
@@ -0,0 +1,3 @@
+document.querySelector('#shortcut').onclick=function(){
+    document.querySelector('#shortcut').innerHTML =  '<img src="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" width=auto height=600 />';
+}
