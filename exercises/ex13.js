11a340d34a66558929ee59e8f90b5461eec6a170
sunjus <sunju.shin1108@gmail.com>
2020-06-10 15:30:18 +0300

    ex13_arrayPushShift

---
+++ exercises/ex13.js
@@ -0,0 +1,11 @@
+let genre = ["Jazz","Blues"]
+console.log(genre)
+genre.push("Rock-n-Roll")
+console.log(genre)
+genre[Math.floor(genre.length/2)]="classics"
+console.log(genre)
+genre.shift
+console.log(genre)
+genre.unshift("Rap","Reggae")
+console.log(genre)
+
