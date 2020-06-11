d3006bdc2bf51d31d4298fbfde58d3b2817a01fc
sunjus <sunju.shin1108@gmail.com>
2020-06-11 15:56:45 +0300

    ex16_slice_sort

---
+++ exercises/ex16.js
@@ -0,0 +1,10 @@
+let webTech = ["HTML", "JavaScript", "CSS"];
+
+function copySorted(webTech) {
+     return webTech.slice().sort()
+ }
+
+let orderedWebTech = copySorted(webTech);
+
+console.log(webTech);
+console.log(orderedWebTech);
