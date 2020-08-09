1e35b4631830f63c8deaf5c6701e9ced2fd3a8e6
sunjus <sunju.shin1108@gmail.com>
2020-08-10 00:08:37 +0300

    Add first version of my portfolio

---
+++ assignment/portfolio-pc/script.js
@@ -0,0 +1,38 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
+$(document).ready(function () {
+  let bottom = $("header").outerHeight();
+  let header = $("header").offset().top + bottom;
+
+  $(window).scroll(function () {
+    if ($(window).width() > 991) {
+      if ($(window).scrollTop() >= header) {
+        $("header").addClass("sticky");
+      } else {
+        $("header").removeClass("sticky");
+      }
+    }
+  });
+
+  $(".navbar ul li a").click(function () {
+    $(".navbar ul li a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".ham-burger").click(function () {
+    $(this).toggleClass("active");
+    $(".navbar").toggleClass("open");
+  });
+});
+
+AOS.init({
+  duration: 1200,
+});
