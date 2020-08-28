472d677ec850142c9f6fdf54bd9d3732ed154f8a
sunjus <sunju.shin1108@gmail.com>
2020-08-28 23:38:52 +0300

    init frontend

---
+++ fullstack/fullstack-app2/frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default (Dashboard) => {
+  return <div>Hello from Dashboard</div>;
+};
---
+++ fullstack/fullstack-app2/frontend/src/pages/Login/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default (Login) => {
+  return <div>Hello from Login</div>;
+};
